4413ed934146f7bac5ce1c1b249d42bd2bfb3e19
aeju <aejuchoe@naver.com>
2023-02-21 22:41:24 +0900

    수 자료형 : 정수형, 실수형, round() 함수

--- DataType.py
+++ DataType.py
@@ -28,3 +28,32 @@ print(a)
 a = -.7
 print(a)
 
+# 10억의 지수 표현 방식
+a = 1e9
+print(a)
+
+# 752.5
+a = 75.25e1
+print(a)
+
+# 3.954
+a = 3954e-3
+print(a)
+
+a = 0.3 + 0.6
+print(a)
+
+if a == 0.9:
+    print(True)
+else:
+    print(False)
+
+# round() : 소수점 특정 자릿수에서 반올림하는 함수
+
+#print("round(a, 4) : " + round(a, 4))
+print(round(a, 4))
+
+if round(a, 4) == 0.9:
+    print(True)
+else:
+    print(False)
