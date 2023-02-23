e9ff857a2db5433c0e97479ee917727c50b21bc4
aeju <aejuchoe@naver.com>
2023-02-23 23:10:21 +0900

    문자열(string) : 초기화, 연산

---
+++ 1. DataType/String.py
@@ -0,0 +1,18 @@
+# 문자열 초기화
+data = 'Hello World'
+print(data)
+
+# /" = "
+data = "Don't you know \"Python\"?"
+print(data)
+
+
+# 문자열 연산
+a = "Hello"
+b = "World"
+
+a = "String"
+print(a * 3)
+
+a = "ABCDEF"
+print(a[2:4]) # : 두번째 숫자-1까지!
