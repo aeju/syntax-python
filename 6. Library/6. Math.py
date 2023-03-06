f600f65cc4ca6d9e6b850e73f177424b0d203202
aeju <aejuchoe@naver.com>
2023-03-06 23:42:56 +0900

    라이브러리-5.math: factorial(x), sqrt(x), gcd(a, b), pi, e

---
+++ 6. Library/6. Math.py
@@ -0,0 +1,26 @@
+# math : 수학적인 기능(팩토리얼, 제곱근, 최대공약수)
+
+
+# 1. factorial(x) : x!
+import math
+
+print(math.factorial(5)) # 5 팩토리얼을 출력
+
+
+# 2. sqrt(x) : x의 제곱근
+import math
+
+print(math.sqrt(7))
+
+
+# 3. gcd(a, b) : 최대 공약수
+import math
+
+print(math.gcd(21, 14))
+
+
+# 4. 상수(pi, e)
+import math
+
+print(math.pi) # 파이 출력
+print(math.e) # 자연상수 e
